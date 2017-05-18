8839115bbd7c8f892a57169ea8ed2e6d6340d0af
Sulaiman404 <Sulaiman.ahmed.95@gmail.com>
2017-05-18 11:08:41 +0300

    Update conn.php

--- inc/conn.php
+++ inc/conn.php
@@ -2,14 +2,15 @@
 
 //$host = 'localhost';
 //$host = 't2017051011.scm.azurewebsites.net';
-$host = 'MYSQLCONNSTR_localdb';
+//$host = '';
 //$user = 'T2017051011';
 //$pass = '12bd43d9ca';
-$user = 'azure';
-$pass = 'password';
-$db = 'T2017051011';
+//$user = 'azure';
+//$pass = 'password';
+//$db = 'T2017051011';
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+//$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect(MYSQLCONNSTR_localdb);
 
 if (mysqli_connect_errno()){
   die("Failed to connect to MySQL: " . mysqli_connect_error());
