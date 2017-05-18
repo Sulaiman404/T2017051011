b57e2f0b5ae50d5262731f990224ea45337c46f3
Sulaiman404 <Sulaiman.ahmed.95@gmail.com>
2017-05-18 11:10:13 +0300

    Update conn.php

--- inc/conn.php
+++ inc/conn.php
@@ -10,10 +10,36 @@
 //$db = 'T2017051011';
 
 //$conn = mysqli_connect($host, $user, $pass, $db);
-$conn = mysqli_connect(MYSQLCONNSTR_localdb);
+//$conn = mysqli_connect(MYSQLCONNSTR_localdb);
 
-if (mysqli_connect_errno()){
-  die("Failed to connect to MySQL: " . mysqli_connect_error());
+//if (mysqli_connect_errno()){
+ // die("Failed to connect to MySQL: " . mysqli_connect_error());
+//}
+
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
+
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
 
 ?>
